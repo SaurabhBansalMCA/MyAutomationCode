866d730b1a92dcaab46560dd39b3906c10392f86
SaurabhBansalMCA <saurabhbansal.mca@gmail.com>
2018-04-11 21:08:54 +0530

    Initial Commit

---
+++ src/main/java/com/cdl/automator/App.java
@@ -0,0 +1,13 @@
+package com.cdl.automator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
